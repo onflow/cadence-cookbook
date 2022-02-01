dfe9b3dedd33cc9597fc6990bfafb1953c555961
Tyllen Bicakcic <tyllenbicakcic@Tyllens-MacBook-Pro.local>
2022-02-01 11:50:31 -0700

    type

--- src/examples/FlowExamples.js
+++ src/examples/FlowExamples.js
@@ -461,7 +461,7 @@ export const flowExamples = [
 
         Remember when we added our play to the set we intialized the moments as 0 so when you mint a moment it will add 1 to that minted moment per play. Before we mint however, we check to see if the play exists in the set or if the play is retired.
 
-        If not, we then get the number of momemnts minted for this play and store that number variable in numInPlay. We would then mint a new Moment as an NFT resource type. We would send in all the parameters specified for the NFT and once we mint that new Moment we then increase the number of moments minted for this play by one.
+        If not, we then get the number of moments minted for this play and store that number variable in numInPlay. We would then mint a new Moment as an NFT resource type. We would send in all the parameters specified for the NFT and once we mint that new Moment we then increase the number of moments minted for this play by one.
 
         We then return the moment with is of an NFT resource type, to later be stored in a collection in a receivers account.
 
