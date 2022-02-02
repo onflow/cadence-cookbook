19c64a2cd235a9d9b473196636ce4eccee19931d
Tyllen Bicakcic <tyllenbicakcic@Tyllens-MacBook-Pro.local>
2022-02-02 13:34:48 -0700

    cleaning up

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,14 +4,11 @@ import logo from '../OClogo3.png'
 
 function Nav() {
     return (
-        <div className='flex border-b-2 border-slate-100 py-3 '>
+        <div className='flex border-b-2 border-slate-100 py-3'>
             <img src={logo} className=' h-10 ml-6' alt="logo" />
-            <div className='flex pt-2 ml-3 text-slate-900'>
+            <div className='flex pt-2 ml-3 text-slate-900 w-full'>
                 <div className="pl-6 hover:font-medium hover:text-green-400">
                     <Link to="/">Examples</Link>
-                </div>
-                <div className=" pl-6 hover:font-medium hover:text-green-400">
-                {/* <Link to="/marketplace-bestpractices">Marketplace Best Practices</Link> */}
                 </div>
             </div>
         </div>
