b7ff9187915dc60818fc454e6ebe11c87f27f060
Tyllen Bicakcic <tyllenbicakcic@Tyllens-MacBook-Pro.local>
2022-02-02 10:23:28 -0700

    remove best practices page for now

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import './App.css';
 import Nav from './components/Nav';
 import PreviewBox from './components/MainExampleComponents/PreviewBox';
-import { flowExamples } from './examples/FlowExamples';
 import {
   Route,
   Routes
 } from "react-router-dom";
-import MarketplaceBP from './components/BestPractices/MarketplaceBP';
 
 
 function App() {
 
   return (
     <div className="App">
-      {/* <Router>
-      
-          <Route exact path="/"> */}
           <Nav />
             <Routes>
               <Route path="/" exact element={<PreviewBox />} />
-              <Route path="/marketplace-bestpractices" exact element={<MarketplaceBP />} />
-
             </Routes>
-            {/* // <PreviewBox flowExamples={flowExamples} /> */}
-          {/* </Route>
-      </Router> */}
-
-      {/* <Searchbox /> */}
-      
     </div>
   );
 }
