e13ef1f6e190516364951e15d583650161594c82
Kimberly Luu <kimcodeashian@gmail.com>
2022-02-10 17:08:08 -0500

    Modal header responsive

--- src/components/MainExampleComponents/Modal.js
+++ src/components/MainExampleComponents/Modal.js
@@ -14,31 +14,35 @@ return (
 
             <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:align-middle w-5/6 sm:max-h-fit">
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:align-middle w-[96%] md:w-5/6 sm:max-h-fit">
                 {/* this is the top part of the modal */}
-                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 overflow-auto max-w-6xl divide-y-[1px] divide-gray-200" style={{maxHeight: '93vh'}}>
-                    <div className="ml-0 mb-5 flex ">
-                        <div className=' w-14 rounded-md' style={{background: `${data.BgColor}`}}>
-                            {/* icon for image will go here */}
-                            <div className='p-3 ml-[5px] mt-[5px]'>
-                            {data.Icon}
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 overflow-auto divide-y-[1px] divide-gray-200" style={{maxHeight: '93vh'}}>
+                    <div className="max-w-lg lg:mx-auto ml-0 mb-5 flex flex-col "> 
+                        
+                        <div className="w-full flex items-center flex-wrap">
+                            <div className=' w-14 h-14 rounded-md' style={{background: `${data.BgColor}`}}>
+                                {/* icon for image will go here */}
+                                <div className='flex flex-col h-full justify-center items-center'>
+                                {data.Icon}
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                            <h3 className="text-2xl leading-6 font-medium text-gray-900 ml-3 mt-0" id="modal-title">{data.ExampleName}</h3>
-                            <div className='ml-3 border-2 p-[1px] pl-1 w-fit pr-1 mt-[5px] border-gray-200 text-gray-400 text-sm rounded-[5px]'>
-                                {data.Category}
+                            <div className="ml-3 mr-auto md:mr-0'">
+                                <h3 className="text-2xl leading-6 font-medium text-gray-900 ml-3 mt-0" id="modal-title">{data.ExampleName}</h3>
+                                <div className='ml-3 border-2 p-[1px] pl-1 w-fit pr-1 mt-[5px] border-gray-200 text-gray-400 text-sm rounded-[5px]'>
+                                    {data.Category}
+                                </div>
                             </div>
-                        </div>
-                        <div className="ml-auto">
-                            <div className='ml-auto text-right'>
-                                <button className='bg-green-500 text-white font-xs py-1 px-3 rounded-[5px] mt-1 right hover:bg-green-600'
-                                onClick={() => window.open(`${data.PlaygroundLink}`, "_blank")}
-                                >
-                                    Open In Playground*
-                                </button>
+                            <div className='md:ml-auto mt-5 md:mt-0 text-right'>
+                                    <button className='bg-green-500 text-white font-xs py-1 px-3 rounded-[5px] mt-1 right hover:bg-green-600'
+                                    onClick={() => window.open(`${data.PlaygroundLink}`, "_blank")}
+                                    >
+                                        Open In Playground*
+                                    </button>
+                                </div>
                             </div>
-                            <div className="text-[12px] text-gray-500 mt-1">
+                        <div className="">
+                            
+                            <div className="text-[12px] text-gray-500 mt-5">
                                 *Please deploy contracts on all necessary accounts before starting transactions and scripts
                             </div>
                         </div>
@@ -73,7 +77,7 @@ return (
                                 </div>
                                 <div>
                                 {/* second div flexed */}
-                                <div className="mt-0 text-center sm:mt-0 xl:ml-10 sm:text-left flex-none ">
+                                <div className="mt-0 sm:mt-0 xl:ml-10 text-left flex-none">
                                     <div className='p-3 whitespace-pre-line mt-10 xl:mt-[70px] bg-indigo-50 rounded-md'>
                                             <h3 className="text-lg leading-6 font-medium text-indigo-900 flex" id="modal-title"><InformationCircleIcon className="h-5 w-5 mt-[1px] mr-1"/> Cadence Code Explanation</h3>
                                             <div className="mt-6">
@@ -86,7 +90,7 @@ return (
                         </div>
                         <div className='col-span-2 mt-4'>
                             <div className='flex flex flex-col xl:flex-row'>
-                                <div className="mt-4 text-center sm:mt-0 sm:text-left xl:min-w-[780px] xl:max-w-[780px]">
+                                <div className="mt-4 sm:mt-0 sm:text-left xl:min-w-[780px] xl:max-w-[780px]">
                                     {/* Cadence code div */}
                                     <div className="mt-6">
                                         <p className="text-md text-black-500 mb-4 font-medium">{data?.Script === true ? "Script Code" : "Transaction Code"}</p>
@@ -102,7 +106,7 @@ return (
                                     </div>
                                 </div>
                                 <div className='mt-[62px]'>
-                                    <div className="mt-0 text-center sm:mt-0 xl:ml-10 sm:text-left">
+                                    <div className="mt-0 sm:mt-0 xl:ml-10">
                                         <div className='p-3 whitespace-pre-line bg-green-50 rounded-[5px]'>
                                             <h3 className="text-lg leading-6 font-medium text-green-900 flex" id="modal-title"><InformationCircleIcon className="h-5 w-5 mt-[1px] mr-1"/>{data?.Script === true ? "Script Explained" : "Transaction Explained"}</h3>
                                             <div className="mt-6">
