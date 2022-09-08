2963e50f738854fdab52695cd87167308b37cf92
Dylan Tinianov <dylantinianov@gmail.com>
2022-09-08 15:57:11 -0400

    Update playground links

--- src/modules/Create_Marketplace/description/description.js
+++ src/modules/Create_Marketplace/description/description.js
@@ -7,7 +7,7 @@ export const name = "Create A Marketplace"
 
 export const description = "Create a secondary marketplace for your NFT's using the NFT Storefront Contract."
 
-export const playgroundLink = "https://play.onflow.org/1d11f838-fc0e-4e7f-86e3-6d1a5a1098e3?type=tx&id=f32eaf35-c6e1-4195-b144-27eef3abda76&storage=none"
+export const playgroundLink = "https://play.onflow.org/9f2f0aee-490e-4ffb-8f42-1d1067949dee?type=account&id=84aa1c58-bb11-4d12-aab4-5de3c3d0857e&storage=none"
 
 export const category = "Marketplace"
 
