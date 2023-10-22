8863d00efb749274cbbac9e003a747e3c860a5cc
lealobanov <lea.lobanov@icloud.com>
2023-10-22 22:36:55 +0900

    Removing unused code

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,44 +28,6 @@ import ProgressBar from 'src/components/progress-bar';
 import { MotionLazy } from 'src/components/animate/motion-lazy';
 import { SettingsProvider, SettingsDrawer } from 'src/components/settings';
 
-
-export const metadata = {
-  title: 'Minimal UI Kit',
-  description:
-    'The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
-  keywords: 'react,material,kit,application,dashboard,admin,template',
-  themeColor: '#000000',
-  manifest: '/manifest.json',
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-  },
-  icons: [
-    {
-      rel: 'icon',
-      url: '/favicon/favicon.ico',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      url: '/favicon/favicon-16x16.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      url: '/favicon/favicon-32x32.png',
-    },
-    {
-      rel: 'apple-touch-icon',
-      sizes: '180x180',
-      url: '/favicon/apple-touch-icon.png',
-    },
-  ],
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={primaryFont.className}>
--- src/app/loading.js
+++ src/app/loading.js
@@ -3,8 +3,6 @@
 // components
 import { SplashScreen } from 'src/components/loading-screen';
 
-// ----------------------------------------------------------------------
-
 export default function Loading() {
   return <SplashScreen />;
 }
