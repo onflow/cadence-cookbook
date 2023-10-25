44703373b9c42facf0c464ba3138a83f10750ed2
lealobanov <lea.lobanov@icloud.com>
2023-10-25 20:11:39 +0900

    Cleanup unused files

--- src/_mock/_blog.js
+++ src/_mock/_blog.js
@@ -1,14 +1,3 @@
-export const POST_PUBLISH_OPTIONS = [
-  {
-    value: 'published',
-    label: 'Published',
-  },
-  {
-    value: 'draft',
-    label: 'Draft',
-  },
-];
-
 export const POST_SORT_OPTIONS = [
   { value: 'latest', label: 'Latest' },
   { value: 'oldest', label: 'Oldest' },
--- src/_mock/index.js
+++ src/_mock/index.js
@@ -1,10 +1,1 @@
-export * from './assets';
-export * from './_mock';
-
 export * from './_blog';
-export * from './_files';
-export * from './_order';
-export * from './_others';
-export * from './_invoice';
-export * from './_product';
-export * from './_overview';
