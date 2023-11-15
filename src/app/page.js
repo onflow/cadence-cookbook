01c5711fafbeba0feec2e58cb5d7a5d8d943c7b6
lealobanov <lea.lobanov@icloud.com>
2023-11-15 23:44:14 +0900

    Metadata seo

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,45 @@
-import { getAllRecipes } from '../data/recipeUtils';
-import { featuredRecipes } from '../data/recipes';
-import { HomePageView } from '../sections/blog/view';
+import { getAllRecipes } from "../data/recipeUtils";
+import { featuredRecipes } from "../data/recipes";
+import { HomePageView } from "../sections/blog/view";
+
+const keywords = [
+  "Cadence Cookbook",
+  "Flow blockchain",
+  "Flow",
+  "Blockchain development",
+  "Cadence",
+  "Cadence programming",
+  "Blockchain development",
+  "Cadence examples",
+  "Cadence use cases",
+  "Flow blockchain examples",
+  "Flow blockchain use cases",
+];
 
 export const metadata = {
-  title: 'Cadence Cookbook | Home',
+  title: `Home | Cadence Cookbook `,
+  description: `Explore Cadence smart contracts and transaction scripts for different use cases on the Flow Blockchain in the Cadence Cookbook.`,
+  keywords: keywords,
+  alternates: {
+    canonical: `https://cookbook.flow.com/`,
+  },
+  openGraph: {
+    title: `Home | Cadence Cookbook `,
+    description: `Explore Cadence smart contracts and transaction scripts for different use cases on the Flow Blockchain in the Cadence Cookbook.`,
+    url: `https://cookbook.flow.com/`,
+    keywords: keywords,
+    images: [
+      {
+        url: `https://cookbook.flow.com/assets/logo/flow_logo.svg`,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default async function HomePage() {
   const recipes = await getAllRecipes();
 
-  return <HomePageView recipes={recipes} featuredRecipes={featuredRecipes}/>;
+  return <HomePageView recipes={recipes} featuredRecipes={featuredRecipes} />;
 }
